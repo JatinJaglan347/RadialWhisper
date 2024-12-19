5b21bc93038c9e2a4b696773ea087ded851df4c1
jatinjaglan347 <jatinjaglan347@gmail.com>
2024-12-19 23:35:54 +0530

    `Added cookie-parser and cors dependencies, updated package-lock.json and package.json, and modified app.js and index.js to use these dependencies. creating utils`

--- server/src/index.js
+++ server/src/index.js
@@ -6,4 +6,13 @@ dotenv.config({
     path: "./.env"
 });
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+})
+.catch((error) => {
+    console.log("Error connecting to MongoDB", error);
+    process.exit(1);
+});
