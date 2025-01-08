dc22cf73a6cd88380bb80dfbeefc9b2420559558
Jatin Jaglan <jatinjaglan347@gmail.com>
2025-01-08 17:56:15 +0530

    `Update axios baseURL to use VITE_API_BASE_URL`

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
     // baseURL: import.meta.env.VITE_API_BASE_URL,
-    baseURL: "http://localhost:5500",
+    baseURL: import.meta.env.VITE_API_BASE_URL,
     withCredentials :true
 })
